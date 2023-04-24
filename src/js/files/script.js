7825483dd88c6be5a09bb0f31d8098ca1d890701
tiwwwka <jolyfish23@gmail.com>
2023-04-24 18:07:09 +0200

    add map

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,11 +1,10 @@
 // Підключення функціоналу "Чертоги Фрілансера"
-import { isMobile } from './functions.js'
+import { isMobile } from './functions.js';
 // Підключення списку активних модулів
-import { flsModules } from './modules.js'
+import { flsModules } from './modules.js';
 
-let marker = document.querySelector('.marker')
-let item = document.querySelectorAll('.menu__item')
-
+// let marker = document.querySelector('.marker')
+// let item = document.querySelectorAll('.menu__item')
 // function indicator(navbarElement) {
 // 	marker.style.left = navbarElement.offsetLeft + 'px'
 // 	marker.style.width = navbarElement.offsetWidth + 'px'
@@ -17,3 +16,21 @@ let item = document.querySelectorAll('.menu__item')
 // 		marker.querySelector('.marker__wrapper').classList.add('_active')
 // 	})
 // })
+
+let map = document.querySelector('.map');
+let places = document.querySelectorAll('.map__box svg path.active');
+
+map.addEventListener('mouseover', (e) => {
+  let targetItem = e.target;
+  if (targetItem.classList.contains('map__place')) {
+    places.forEach((place) => {
+      if (targetItem.dataset.map == place.id) {
+        place.classList.add('filled');
+      } else {
+        place.classList.remove('filled');
+      }
+    });
+  } else {
+    places.forEach((place) => place.classList.remove('filled'));
+  }
+});
