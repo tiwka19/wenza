29041c0c9c98c32e0d5c3f70211907819b34a3df
WIN-KN7RUO6E1HC\user <jolyfish23@gmail.com>
2023-05-14 12:25:25 +0300

    improvement showmore js

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -6,44 +6,50 @@ import ScrollReveal from 'scrollreveal';
 import Toastify from 'toastify-js';
 import JustValidate from 'just-validate';
 
-let map = document.querySelector('.map');
-let places = document.querySelectorAll('.map__box svg path.active');
+const mapFunc = () => {
+  let map = document.querySelector('.map');
+  let places = document.querySelectorAll('.map__box svg path.active');
 
-map.addEventListener('mouseover', (e) => {
-  let targetItem = e.target;
-  if (targetItem.classList.contains('map__place')) {
-    places.forEach((place) => {
-      if (targetItem.dataset.map == place.id) {
-        place.classList.add('filled');
-      } else {
-        place.classList.remove('filled');
-      }
-    });
-  } else {
-    places.forEach((place) => place.classList.remove('filled'));
-  }
-});
-
-if (window.matchMedia('(max-width: 1300px)').matches) {
-  map.addEventListener('click', (e) => {
+  if(!map) return;
+  
+  map.addEventListener('mouseover', (e) => {
     let targetItem = e.target;
     if (targetItem.classList.contains('map__place')) {
       places.forEach((place) => {
-        if (targetItem.dataset.map === place.id) {
+        if (targetItem.dataset.map == place.id) {
           place.classList.add('filled');
-          let event = new MouseEvent('click', {
-            view: window,
-            bubbles: true,
-            cancelable: true,
-          });
-          place.dispatchEvent(event);
         } else {
           place.classList.remove('filled');
         }
       });
-    } 
+    } else {
+      places.forEach((place) => place.classList.remove('filled'));
+    }
   });
+  
+  if (window.matchMedia('(max-width: 1300px)').matches) {
+    map.addEventListener('click', (e) => {
+      let targetItem = e.target;
+      if (targetItem.classList.contains('map__place')) {
+        places.forEach((place) => {
+          if (targetItem.dataset.map === place.id) {
+            place.classList.add('filled');
+            let event = new MouseEvent('click', {
+              view: window,
+              bubbles: true,
+              cancelable: true,
+            });
+            place.dispatchEvent(event);
+          } else {
+            place.classList.remove('filled');
+          }
+        });
+      } 
+    });
+  }
 }
+
+
 
 const callbackInputs = document.querySelectorAll('.callback__input');
 callbackInputs.forEach((input) => {
